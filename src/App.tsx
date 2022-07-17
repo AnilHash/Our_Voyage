769bff7d47255175979839ff919c6e1e22a34947
AnilHash <anilaraganji123@gmail.com>
2022-07-18 01:05:45 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useState } from "react";
+
+import Modal from "./components/Modal";
+import Nav_Bar from "./components/Nav_Bar";
+import BlogList from "./pages/BlogList";
+import { BlogArr, BlogContextType } from "./types";
+
+export const BlogContext = createContext<BlogContextType | null>(null);
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [blogs, setBlogs] = useState<BlogArr>([]);
+  const [editBlogId, setEditBlogId] = useState<string>();
+  const handleEdit = (id: string) => {
+    setEditBlogId(id);
+    setShowModal(!showModal);
+  };
+  const toggleModal = () => setShowModal(!showModal);
+  return (
+    <div className="container-fluid gx-0">
+      <BlogContext.Provider
+        value={{
+          blogs,
+          setBlogs,
+          toggleModal,
+          showModal,
+          editBlogId,
+          setEditBlogId,
+        }}
+      >
+        <Nav_Bar toggleModal={toggleModal} />
+        {/* <Container /> */}
+        <BlogList handleEdit={handleEdit} />
+        {showModal && <Modal />}
+      </BlogContext.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface BlogType {
+  _id: string;
+  title: string;
+  body: string;
+}
+
+export type BlogArr = BlogType[];
+
+export interface BlogContextType {
+  blogs: BlogArr;
+  setBlogs: React.Dispatch<React.SetStateAction<BlogArr>>;
+  setEditBlogId: React.Dispatch<React.SetStateAction<string | undefined>>;
+  toggleModal: () => void;
+  showModal: boolean;
+  editBlogId: string | undefined;
+}
+
+export interface BlogListPropType {
+  handleEdit: (id: string) => void;
+}
